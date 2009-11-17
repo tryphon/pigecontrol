fa6298b6c34b4d0e8107e79b561d885be7677fd5
Alban Peignier <alban.peignier@free.fr>
2009-11-17 10:50:39 +0100

    Add Chunck#records and first validations

--- app/models/chunk.rb
+++ app/models/chunk.rb
@@ -1,3 +1,13 @@
 class Chunk < ActiveRecord::Base
   belongs_to :source
+
+  validates_presence_of :begin, :end
+
+  def records
+    if self.begin and self.end
+      self.source.records.including(self.begin, self.end)
+    else
+      []
+    end
+  end
 end
--- spec/models/chunk_spec.rb
+++ spec/models/chunk_spec.rb
@@ -1,16 +1,38 @@
 require 'spec_helper'
 
 describe Chunk do
+
   before(:each) do
-    @valid_attributes = {
-      :begin => Time.now,
-      :end => Time.now,
-      :completion_rate => 1.5,
-      :source_id => 1
-    }
+    @chunk = Factory(:chunk)
   end
 
-  it "should create a new instance given valid attributes" do
-    Chunk.create!(@valid_attributes)
+  it { should validate_presence_of(:begin) }
+  it { should validate_presence_of(:end) }
+
+  it { should belong_to(:source) }
+
+  describe "records" do
+
+    before(:each) do
+      @chunk.stub!(:source).and_return(mock(Source, :records => mock("source records")))
+      @records = Array.new(3) { mock Record }
+    end
+    
+    it "should retrieve source recordings including chunck begin and end" do
+      @chunk.source.records.should_receive(:including).with(@chunk.begin, @chunk.end).and_return(@records)
+      @chunk.records.should == @records
+    end
+
+    it "should return an empty array when begin is not defined" do
+      @chunk.begin = nil
+      @chunk.records.should be_empty
+    end
+
+    it "should return an empty array when end is not defined" do
+      @chunk.end = nil
+      @chunk.records.should be_empty
+    end
+    
   end
+
 end
