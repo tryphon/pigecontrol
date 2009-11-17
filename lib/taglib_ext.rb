4c2d408f4e3bd151d057a0291613eb9eaa521edb
Alban Peignier <alban.peignier@free.fr>
2009-11-17 17:05:45 +0100

    Improve TagLib::File to return a length for wave file

--- lib/taglib_ext.rb
+++ lib/taglib_ext.rb
@@ -1,5 +1,6 @@
 module TagLib
   class File
+
     def self.open(filename, &block)
       file = TagLib::File.new(filename)
 
@@ -9,5 +10,19 @@ module TagLib
         file.close
       end
     end
+
+    def size
+      ::File.size(@path)
+    end
+
+    def length_with_wav_support
+      if @path =~ /\.wav/i
+         self.size / (self.bitrate / 8 * 1024)
+      else
+        length_without_wav_support
+      end
+    end
+    alias_method_chain :length, :wav_support
+    
   end
 end
---
+++ spec/lib/taglib_ext_spec.rb
@@ -0,0 +1,34 @@
+require 'spec_helper'
+
+describe TagLib::File, "open" do
+
+  before(:each) do
+    @filename = "filename"
+
+    @taglib_file = mock(TagLib::File, :close => nil)
+    TagLib::File.stub!(:new).and_return(@taglib_file)
+  end
+
+  it "should create a TagLib::File with given filename" do
+    TagLib::File.should_receive(:new).with(@filename)
+    TagLib::File.open(@filename) {}
+  end
+
+  it "should yield the given block with the TagLib::File and returns the block result" do
+    TagLib::File.open(@filename) { |file| file }.should == @taglib_file
+  end
+
+  it "should close the TagLib::File" do
+    @taglib_file.should_receive(:close)
+    TagLib::File.open(@filename) {}
+  end
+
+end
+
+describe TagLib::File do
+  
+  it "should support wav files" do
+    TagLib::File.open(test_file(:wav)) { |f| f.length }.should > 0
+  end
+
+end
