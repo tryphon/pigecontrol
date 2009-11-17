0a02de9dd94dbb959d8033f4fd80ddf78beaaec7
Alban Peignier <alban.peignier@free.fr>
2009-11-17 19:29:59 +0100

    Implement Chunk#create_file!

--- app/models/chunk.rb
+++ app/models/chunk.rb
@@ -10,4 +10,31 @@ class Chunk < ActiveRecord::Base
       []
     end
   end
+
+  def self.storage_directory
+    unless @storage_directory
+      @storage_directory = "#{Rails.root}/tmp/chunks"
+      FileUtils.mkdir_p @storage_directory
+    end
+
+    @storage_directory
+  end
+
+  def file
+    filename if File.exist?(filename)
+  end
+
+  def filename
+    @filename ||= "#{Chunk.storage_directory}/#{self.id}.wav"
+  end
+
+  def create_file!
+    Sox.command do |sox|
+      records.each do |record|
+        sox.input record.filename
+      end
+      sox.output filename
+    end
+  end
+
 end
--- spec/models/chunk_spec.rb
+++ spec/models/chunk_spec.rb
@@ -35,4 +35,55 @@ describe Chunk do
     
   end
 
+  describe "filename" do
+
+    it "should be :storage_directory/:id.wav" do
+      Chunk.stub!(:storage_directory).and_return("storage_directory")
+      @chunk.id = 1
+
+      @chunk.filename.should == "storage_directory/1.wav"
+    end
+
+  end
+
+  describe "file" do
+
+    it "should return the file containing chunk content if exists" do
+      File.should_receive(:exist?).with(@chunk.filename).and_return(true)
+      @chunk.file.should == @chunk.filename
+    end
+
+    it "should be nil if file doesn't exist" do
+      File.stub!(:exist?).and_return(false)
+      @chunk.file.should be_nil
+    end
+
+  end
+
+  describe "create_file!" do
+
+    before(:each) do
+      @sox = mock(Sox::Command, :input => nil, :output => nil)
+      Sox.stub!(:command).and_yield(@sox)
+      
+      @records = Array.new(3) do |n| 
+        mock(Record, :filename => "file#{n}")
+      end
+      @chunk.stub!(:records).and_return(@records)
+    end
+
+    it "should use record files as sox inputs" do
+      @records.each do |record|
+        @sox.should_receive(:input).with(record.filename)
+      end
+      @chunk.create_file!
+    end
+
+    it "should use chunk filename as sox output" do
+      @sox.should_receive(:output).with(@chunk.filename)
+      @chunk.create_file!
+    end
+
+  end
+
 end
