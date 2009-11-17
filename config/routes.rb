2fdad9d4767dc76436588b6a08220d8bf9aaffbb
Alban Peignier <alban.peignier@free.fr>
2009-11-17 17:53:38 +0100

    Define a root route

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ ActionController::Routing::Routes.draw do |map|
   map.resources :sources do |source|
     source.resources :chunks
   end
+
+  map.root :controller => "chunks", :action => "index", :source_id => "1"
 end
