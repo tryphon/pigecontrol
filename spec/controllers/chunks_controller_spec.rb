c18cc3fdea3a19623398ee13f3722630a748cd74
Alban Peignier <alban.peignier@free.fr>
2009-11-18 11:23:36 +0100

    Add ChunksController spec

---
+++ spec/controllers/chunks_controller_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+describe ChunksController do
+
+  describe "GET show in wav" do
+
+    before(:each) do
+      @chunk = Factory(:chunk)
+    end
+
+    it "assigns the requested chunk as @chunk" do
+      File.stub!(:exists?).and_return(true)
+      controller.should_receive(:send_file).with(@chunk.file)
+
+      get :show, :id => @chunk, :source_id => @chunk.source, :format => "wav"
+    end
+
+  end
+
+end
