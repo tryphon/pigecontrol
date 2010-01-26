0bbad31f8456757c4ea2d0e65a65a6d70c9a57c3
Alban Peignier <alban@tryphon.eu>
2010-01-26 14:24:56 +0100

    Add Uservoice feedback link to the layout. Fixes #8

---
+++ config/initializers/user_voice.rb
@@ -0,0 +1,10 @@
+UserVoice.config do |account|
+  account.key = 'pigebox'
+  account.host = 'pigebox.uservoice.com'
+  account.forum = 'general' 
+  account.alignment = 'left'
+  account.background_color = '#1273b4' 
+  account.text_color = 'white'
+  account.hover_color = '#00c9ff'
+  account.lang = 'fr'
+end
