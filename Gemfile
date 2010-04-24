61157624ef4ca5fee4dc88e04856ad94265a2893
Alban Peignier <alban@tryphon.eu>
2010-04-24 14:14:50 +0200

    Use delayed_job gem (instead of old github plugin)

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ gem "rails", "2.3.5"
 gem "inherited_resources", "= 1.0.3"
 gem "will_paginate", "~> 2.3.11"
 gem "sqlite3-ruby"
+gem "delayed_job"
 
 group :test do
   gem "factory_girl"
--- config/environment.rb
+++ config/environment.rb
@@ -31,6 +31,7 @@ Rails::Initializer.run do |config|
   # inherited_resources 1.1.0 is only compatible with Rails 3
   config.gem "inherited_resources", :version => "1.0.3"
   config.gem 'will_paginate', :version => '~> 2.3.11', :source => 'http://gemcutter.org'
+  config.gem "delayed_job", :source => 'http://gemcutter.org'
 
   # Only load the plugins named here, in the order given. By default, all plugins 
   # in vendor/plugins are loaded in alphabetical order.
