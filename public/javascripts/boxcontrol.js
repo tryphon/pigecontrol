17d41ee29199c21ae81631a4036970a949c907a0
Alban Peignier <alban@tryphon.eu>
2010-10-18 14:49:32 +0200

    Update boxcontrol and its javascript code

--- public/javascripts/boxcontrol.js
+++ public/javascripts/boxcontrol.js
@@ -18,3 +18,50 @@ Event.observe(window, "load", function() {
     });
   });
 });
+
+/*
+  When a download_pending link is detected, we're checking
+  every 5 seconds the release status (by retrieving the json).
+  When release is downloaded, we're reloading the page.
+*/
+Event.observe(window, "load", function() {
+    download_pending_event = $$('.download-pending').first();
+    if (download_pending_event) {
+        new ReleaseDownloadObserver(download_pending_event.href, download_pending_event.getAttribute("data-release-id"));
+    };
+});
+
+ReleaseDownloadObserver = Class.create({
+    initialize: function(base_url, identifier) {
+        this.base_url = base_url;
+        this.identifier = identifier;
+        this.periodical_executer();
+    },
+    reload_when_downloaded: function(transport) {
+        var release = transport.responseText.evalJSON().release;
+        console.log(release);
+        if (release.status != 'download_pending') {
+            this.reload_page();
+        }
+    },
+    reload_page: function() {
+        window.location = this.base_url;
+    },
+    check_if_release_is_downloaded: function() {
+        new Ajax.Request(this.base_url + "/" + this.identifier + ".json", { 
+            asynchronous: true, evalScripts: true, method: 'get', onSuccess: this.reload_when_downloaded.bind(this)
+        });
+    },
+    update_release_description: function() {
+        new Ajax.Updater({ success: 'release_' + this.identifier }, this.base_url + '/' + this.identifier + "/description", {
+            method: 'get', onFailure: this.reload_page.bind(this)
+        });
+    },
+    update_release: function() {
+        this.check_if_release_is_downloaded();
+        this.update_release_description();
+    },
+    periodical_executer: function() {
+        new PeriodicalExecuter(this.update_release.bind(this), 5);
+    }
+});
