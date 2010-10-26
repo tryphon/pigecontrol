2bc21e84148a278d452a37f751ddda9034cef28b
Alban Peignier <alban@tryphon.eu>
2010-10-27 00:25:22 +0200

    Create fake tmp/current.yml during deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,9 +1,8 @@
 set :application, "pige"
 
-set :repository,  "git://projects.tryphon.eu/pige"
+set :repository,  "git://www.dbx.tryphon.priv/pige"
 set :scm, :git
 set :git_enable_submodules, true
-set :deploy_via, :copy
 
 set :deploy_to, "/var/www/pige"
 
@@ -11,11 +10,11 @@ server "radio.dbx.tryphon.priv", :app, :web, :db, :primary => true
 
 # after "deploy:setup", "db:create"
 
-set :keep_releases, 10
+set :keep_releases, 5
 after "deploy:update", "deploy:cleanup" 
 set :use_sudo, false
 
-after "deploy:update_code", "deploy:symlink_shared", "deploy:gems"
+after "deploy:update_code", "deploy:symlink_shared", "deploy:gems", "deploy:current_yml"
 after "deploy:migrations", "deploy:fix_db_permissions"
 
 namespace :deploy do
@@ -42,6 +41,12 @@ namespace :deploy do
   desc "Install gems"
   task :gems, :roles => :app do
     sudo "rake --rakefile=#{release_path}/Rakefile gems:install RAILS_ENV=production"
+  end
+
+  desc "Create a current.yml file"
+  task :current_yml, :roles => :app do
+    now = Time.now
+    put "pigebox-#{now.strftime("%Y%m%d-%H%M")}\nstatus_updated_at: #{now.strftime('%F %T')}\ndescription_url: http://www.tryphon.eu/products/pigebox", "#{release_path}/tmp/current.yml"
   end
 
   desc "Fix database file permissions"
