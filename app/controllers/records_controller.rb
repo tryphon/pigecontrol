ad35f1ac58b11f90f60bfbfb4f044a373455460f
Alban Peignier <alban@tryphon.eu>
2011-03-08 15:45:13 +0100

    Create a podcast feed for records

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,7 +1,21 @@
 class RecordsController < InheritedResources::Base
   belongs_to :source
 
-  actions :create
-  respond_to :json
+  actions :create, :index, :show
+  respond_to :json, :only => :create
+  respond_to :rss, :only => :index
+
+  def show
+    show! do |format|
+      format.wav { send_file @record.filename, :type => :wav }
+      format.ogg { send_file @record.filename, :type => :ogg }
+    end
+  end
+
+  private
+
+  def collection
+    @records ||= Record.uniq(end_of_association_chain.find(:all, :limit => 20, :order => "end desc"))
+  end
 
 end
