4ac24ab5dfcab51143dbc533685473d6c8d43de3
Alban Peignier <alban@tryphon.eu>
2011-10-09 17:47:25 +0200

    Restore taglib workaround for wav length

--- lib/rtaglib_ext.rb
+++ lib/rtaglib_ext.rb
@@ -13,20 +13,22 @@ module TagFile
       end
     end
 
-    # def size
-    #   ::File.size(@path)
-    # end
+    def size
+      ::File.size(@path)
+    end
 
-    # def length_with_wav_support
-    #   if @path =~ /\.wav/i
-    #     bitrate = self.bitrate
-    #     bitrate = 1378.125 if bitrate == 1378
-    #     self.size / (bitrate / 8 * 1024)
-    #   else
-    #     length_without_wav_support
-    #   end
-    # end
-    # alias_method_chain :length, :wav_support
+    # Workaround
+    # squeeze taglib returns 0 for wav files
+    def length_with_wav_support
+      if @path =~ /\.wav/i
+        bitrate = self.bitrate
+        bitrate = 1378.125 if bitrate == 1378
+        self.size / (bitrate / 8 * 1024)
+      else
+        length_without_wav_support
+      end
+    end
+    alias_method_chain :length, :wav_support
     
   end
 end
