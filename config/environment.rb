e7e071cce915f500c6ac075246582b51fcd6ec98
Alban Peignier <alban@tryphon.eu>
2013-07-19 12:45:05 +0200

    Replace box by tryphon-boc in gem config

--- config/environment.rb
+++ config/environment.rb
@@ -20,7 +20,7 @@ Rails::Initializer.run do |config|
   # you must remove the Active Record framework.
   # config.frameworks -= [ :active_record, :active_resource, :action_mailer ]
 
-  # Specify gems that this application depends on. 
+  # Specify gems that this application depends on.
   # They can then be installed with "rake gems:install" on new installations.
   # You have to specify the :lib option for libraries, where the Gem name (sqlite3-ruby) differs from the file itself (sqlite3)
   config.load_paths << "#{Rails.root}/app/presenters"
@@ -42,9 +42,9 @@ Rails::Initializer.run do |config|
   config.gem 'will_paginate'
   config.gem "delayed_job"
   config.gem "pige"
-  config.gem "box"
+  config.gem "tryphon-box"
 
-  # Only load the plugins named here, in the order given. By default, all plugins 
+  # Only load the plugins named here, in the order given. By default, all plugins
   # in vendor/plugins are loaded in alphabetical order.
   # :all can be used as a placeholder for all plugins not explicitly named
   # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
@@ -68,7 +68,7 @@ Rails::Initializer.run do |config|
 
   # Your secret key for verifying cookie session data integrity.
   # If you change this key, all old sessions will become invalid!
-  # Make sure the secret is at least 30 characters and all random, 
+  # Make sure the secret is at least 30 characters and all random,
   # no regular words or you'll be exposed to dictionary attacks.
   config.action_controller.session = {
     :key => '_pige_session',
