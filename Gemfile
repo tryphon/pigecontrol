ae05700ad371fd9bc1b6d27953dcca7de180f1cf
Alban Peignier <alban@tryphon.eu>
2015-03-30 10:07:36 +0200

    Update tryphon-box and boxcontrol gems

--- Gemfile
+++ Gemfile
@@ -8,7 +8,7 @@ gem 'rails', '~> 3.2.17'
 gem 'sqlite3'
 
 gem 'user_interface', :git => 'git://projects.tryphon.priv/user-interface' #, :path => "~/Projects/UserInterface"
-gem 'boxcontrol', :git => 'git://projects.tryphon.priv/boxcontrol', :require => 'box_control' #, :path => "~/Projects/BoxControl"
+gem 'boxcontrol', :git => 'git://projects.tryphon.priv/boxcontrol', :require => 'box_control'#, :path => "~/Projects/BoxControl"
 gem 'tryphon-box', :git => 'git://projects.tryphon.priv/box'#, :path => "~/Projects/Box"
 gem 'inherited_resources'
 
--- config/box.rb
+++ config/box.rb
@@ -1,18 +1,16 @@
-# if defined?(Rails)
-#   Box.logger = Rails.logger
-# else
-#   require 'logger'
-#   Box.logger = Logger.new("log/box.log")
-# end
-
-# Use explicitly ruby1.9.1 (instead of 'env ruby')
-unless Box.executable.start_with?("/usr/bin/ruby1.9.1")
-  Box.executable = "/usr/bin/ruby1.9.1 #{Box.executable}"
+if defined?(Rails)
+  Box.logger = Rails.logger
+else
+  require 'logger'
+  Box.logger = Logger.new("log/box.log")
 end
 
 Box::Release.latest_url = "http://dev.tryphon.priv/dist/pigebox/latest.yml"
 Box::Release.current_url = "public/current.yml"
 Box::Release.install_command = "/bin/true"
+
+Box::PuppetConfiguration.configuration_file = "tmp/config.pp"
+Box::PuppetConfiguration.deploy_command = "/bin/true"
 
 # Use this script to initialize Box commands executed in background
 Box.start_default_options = { :config => __FILE__ }
